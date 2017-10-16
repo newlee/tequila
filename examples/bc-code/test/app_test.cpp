4218457defd8538cc06647f55e4b265e05f59c9b
newlee <newlee.cn@gmail.com>
2017-10-17 06:50:32 +0800

    refactor test

--- examples/bc-code/test/app_test.cpp
+++ examples/bc-code/test/app_test.cpp
@@ -8,6 +8,10 @@ struct StubCargoProvider : services::CargoProvider{
     int after_days;
     virtual void Confirm(Cargo *cargo) override;
 };
+
+static const int ID = 1;
+static const int AFTER_DAYS = 10;
+
 StubCargoProvider* provider = new StubCargoProvider();
 
 api::Api* createApi()  {
@@ -18,26 +22,30 @@ api::Api* createApi()  {
     return api;
 }
 
+void createCargo(api::CreateCargoMsg* msg) {
+    api::Api* api = createApi();
+    api->CreateCargo(msg);
+}
+
 TEST(bc_demo_test, create_cargo)
 {
-    api::Api* api = createApi();
     api::CreateCargoMsg* msg = new api::CreateCargoMsg();
-    msg->Id = 1;
-    msg->AfterDays = 10;
-    api->CreateCargo(msg);
+    msg->Id = ID;
+    msg->AfterDays = AFTER_DAYS;
+    createCargo(msg);
     EXPECT_EQ(msg->Id, provider->cargo_id);
-    EXPECT_EQ(10, provider->after_days);
+    EXPECT_EQ(msg->AfterDays, provider->after_days);
 }
 
 TEST(bc_demo_test, delay_cargo)
 {
     api::Api* api = createApi();
     api::CreateCargoMsg* msg = new api::CreateCargoMsg();
-    msg->Id = 1;
-    msg->AfterDays = 10;
+    msg->Id = ID;
+    msg->AfterDays = AFTER_DAYS;
     api->CreateCargo(msg);
-    api->Delay(1,2);
-    EXPECT_EQ(msg->Id, provider->cargo_id);
+    api->Delay(ID,2);
+    EXPECT_EQ(ID, provider->cargo_id);
     EXPECT_EQ(12, provider->after_days);
 }
 
